31bc72284e8e080dcfee98aef5c747edc39fb63a
Am0li <102870612+Am0li@users.noreply.github.com>
2024-11-12 23:29:35 +0100

    Connecting to database and migrating models
    Connecting to database and migrating models. Adding example .env file for connecting to data base.

--- server/settings.py
+++ server/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'navigation.apps.NavigationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,11 +81,27 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'mssql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', '1433'),
+
+            'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+            'extra_params': (
+                'Persist Security Info=False;'
+                'MultipleActiveResultSets=False;'
+                'Encrypt=yes;'
+                'TrustServerCertificate=yes;'
+                'Connection Timeout=30;'
+                'Encrypt=False;'
+                ),
+            },
+        },
     }
-}
 
 
 # Password validation
