c459756653a4701b0b80b8f6d03a492f504d5b3b
Aleksandra Hinc <olka.hinc@gmail.com>
2024-11-29 03:10:56 +0100

    refactor: uporządkowanie kodu endpointów #42
    Uporządkowanie kodu dotyczącego endpointów (plików urls.py i views.py):
    - przepisanie metod login_user, logout_user i register_user na widoki oparte na klasach;
    - usunięcie zbędnych fragmentów kodu;
    - poprawienie kolejności klas, ścieżek, importowania;
    - zmiana części metod PUT na PATCH;
    - dodanie brakującego wyjątku do metod klasy Distance_sum_update.

--- navigation/urls.py
+++ navigation/urls.py
@@ -14,20 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import Distance_sum_update, Login, Logout, Object_list, Object_single, Register, Reset_password, Reset_password_request, Route_created_count_update, User_list
 from django.contrib import admin
 from django.urls import path
-from .views import Object_list, login_user, logout_user, register_user, Reset_password, Reset_password_request, User_list, Distance_sum_update, Route_created_count_update, Object_single
 
 urlpatterns = [
     path('admin', admin.site.urls),
+    path('all-users', User_list.as_view(), name='user_list'),
+    path('auth/login', Login.as_view(), name='login'),
+    path('auth/logout', Logout.as_view(), name='logout'),
+    path('auth/register', Register.as_view(), name='register'),
+    path('auth/reset-password', Reset_password.as_view(), name='reset_password'),
+    path('auth/reset-password-request', Reset_password_request.as_view(), name='reset_password_request'),
     path('objects', Object_list.as_view(), name='object_list'),
     path('single-object', Object_single.as_view(), name='object_single'),
-    path('auth/login', login_user, name='login'),
-    path('auth/logout', logout_user, name='logout'),
-    path('auth/register', register_user, name='register'),
-    path('auth/reset-password-request', Reset_password_request.as_view(), name='reset_password_request'),
-    path('auth/reset-password', Reset_password.as_view(), name='reset_password'),
-    path('all-users', User_list.as_view(), name='user_list'),
     path('user-distance-sum', Distance_sum_update.as_view(), name='distance_sum_update'),
     path('user-route-created-count', Route_created_count_update.as_view(), name='route_created_count_update')
 ]
--- navigation/views.py
+++ navigation/views.py
@@ -1,15 +1,100 @@
+from .models import AreaObject, CustomUser, Object, PointObject, UserObjectSearch
+from .serializers import AreaObjectSerializer, ObjectDynamicSerializer, PointObjectSerializer, UserSerializer, UserExtendedSerializer, UserObjectSearchSerializer, UserObjectSearchExtendedSerializer
 from django.core.mail import send_mail
-from rest_framework import generics, status, serializers
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from .serializers import ObjectDynamicSerializer, UserSerializer, UserExtendedSerializer, UserObjectSearchSerializer, UserObjectSearchExtendedSerializer, PointObjectSerializer, AreaObjectSerializer
+from rest_framework import generics, serializers, status
 from rest_framework.authtoken.models import Token
-from .models import CustomUser, Object, UserObjectSearch, PointObject, AreaObject
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+class Distance_sum_update(APIView):
+
+    def get(self, request):
+        try:
+            id = request.data.get('id')
+            user = CustomUser.objects.get(id=id)
+            serializer = UserExtendedSerializer(user, data=request.data, partial=True)
+            if serializer.is_valid():
+                return Response({
+                        "code": 200,
+                        "distance_sum": serializer.data.get("distance_sum"),
+                        }, status=status.HTTP_200_OK)
+        except CustomUser.DoesNotExist:
+                return Response({
+                    "code": 400,
+                    "message": "Incorrect id"
+                    }, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({
+                "code": 500,
+                "message": "Server error"
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def patch(self, request):
+        try:
+            id = request.data.get('id')
+            user = CustomUser.objects.get(id=id)
+            serializer = UserExtendedSerializer(user, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    "code": 200,
+                    "message": "Data updated"
+                    }, status=status.HTTP_200_OK)
+        except CustomUser.DoesNotExist:
+                return Response({
+                    "code": 400,
+                    "message": "Incorrect id"
+                    }, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({
+                "code": 500,
+                "message": "Server error"
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class Login(APIView):
+    
+    def post(self, request):
+        try:
+            email = request.data.get('email')
+            password = request.data.get('password')
+            try:
+                user = CustomUser.objects.get(email=email)
+            except CustomUser.DoesNotExist:
+                return Response({"code": 400, "message": "Invalid login or password"}, status=status.HTTP_400_BAD_REQUEST)
+            if not user.check_password(password):
+                return Response({"code": 400, "message": "Invalid login or password"}, status=status.HTTP_400_BAD_REQUEST)
+            token, _ = Token.objects.get_or_create(user=user)
+            serializer = UserSerializer(instance=user)
+            return Response({
+                                "code": 200,
+                                "message": "Login successful",
+                                "token": token.key,
+                                "userID": serializer.data.get("id")
+                            }, status=status.HTTP_200_OK)
+        except:
+            return Response({
+                    "code": 500,
+                    "message": "Server error"
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class Logout(APIView):
+
+    def post(self, request):
+        token_key = request.data.get('token')
+        if not token_key:
+            return Response({"code": 401, "message": "Sesion or token disactive"}, status=status.HTTP_401_UNAUTHORIZED)
+        try:
+            token = Token.objects.get(key=token_key)
+            token.delete()
+            return Response({"code": 200, "message": "Logout successful"}, status=status.HTTP_200_OK)
+        except Token.DoesNotExist:
+            return Response({"code": 401, "message": "Sesion or token disactive"}, status=status.HTTP_401_UNAUTHORIZED)
+        except:
+            return Response({"code": 500, "message": "Server error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class Object_list(generics.ListAPIView):
 
-    def get(self, request, format=None):
+    def get(self, request):
         try:
             point_objects = PointObject.objects.all()
             point_object_serializer = PointObjectSerializer(point_objects, many=True)
@@ -23,8 +108,9 @@ class Object_list(generics.ListAPIView):
         except:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-class Object_single(generics.ListAPIView):
-    def get(self, request, format=None):
+class Object_single(generics.RetrieveAPIView):
+    
+    def get(self, request):
         try:
             id = request.data.get("id")
             object = Object.objects.get(id=id)
@@ -45,85 +131,36 @@ class Object_single(generics.ListAPIView):
                 "message": "Server error"
                 }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-@api_view(['POST'])
-def login_user(request):
-    try:
-        email = request.data.get('email')
-        password = request.data.get('password')
+class Register(APIView):
+    
+    def post(self, request):
         try:
-            user = CustomUser.objects.get(email=email)
-        except CustomUser.DoesNotExist:
-            return Response({"code": 400, "message": "Invalid login or password"}, status=status.HTTP_400_BAD_REQUEST)
-        if not user.check_password(password):
-            return Response({"code": 400, "message": "Invalid login or password"}, status=status.HTTP_400_BAD_REQUEST)
-        token, created = Token.objects.get_or_create(user=user)
-        serializer = UserSerializer(instance=user)
-        return Response({
-                            "code": 200,
-                            "message": "Login successful",
-                            "token": token.key,
-                            "userID": serializer.data.get("id")
-                        }, status=status.HTTP_200_OK)
-    except:
-        return Response({
-                "code": 500,
-                "message": "Server error"
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-@api_view(['POST'])
-def register_user(request):
-    try:
-        serializer = UserSerializer(data=request.data)
-        # Jeżeli tu sie wywala, usuncie baze danych i stworzcie ponownie
-        if serializer.is_valid():
-            validated_data = serializer.validated_data
-            password = validated_data.pop("password", None)
-            user = CustomUser(**validated_data)
-            if password:
-                user.set_password(password)
-            user.save()
-            token = Token.objects.create(user=user)
+            serializer = UserSerializer(data=request.data)
+            if serializer.is_valid():
+                validated_data = serializer.validated_data
+                password = validated_data.pop("password", None)
+                user = CustomUser(**validated_data)
+                if password:
+                    user.set_password(password)
+                user.save()
+                _ = Token.objects.create(user=user)
+                return Response({
+                    "code": 201,
+                    "message": "Register successful"
+                }, status=status.HTTP_201_CREATED)
             return Response({
-                "code": 201,
-                "message": "Register successful"
-            }, status=status.HTTP_201_CREATED)
-        return Response({
-                "code": 400,
-                "message": "Incorrect e-mail or password format"
-            }, status=status.HTTP_400_BAD_REQUEST)
-    except:
-        return Response({
-                "code": 500,
-                "message": "Server error"
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                    "code": 400,
+                    "message": "Incorrect e-mail or password format"
+                }, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({
+                    "code": 500,
+                    "message": "Server error"
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+class Reset_password(APIView):
 
-@api_view(['POST'])
-def logout_user(request):
-    token_key = request.data.get('token')
-
-    if not token_key:
-        return Response({"code": 401, "message": "Sesion or token disactive"}, status=status.HTTP_401_UNAUTHORIZED)
-    try:
-        token = Token.objects.get(key=token_key)
-        token.delete()
-        return Response({"code": 200, "message": "Logout successful"}, status=status.HTTP_200_OK)
-
-    except Token.DoesNotExist:
-        # Token not found
-        return Response({"code": 401, "message": "Sesion or token disactive"}, status=status.HTTP_401_UNAUTHORIZED)
-
-    except:
-        # Handle unexpected errors
-        return Response({"code": 500, "message": "Server error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-
-class Reset_password(generics.UpdateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
-
-    def put(self, request):
+    def patch(self, request):
         try:
             token_key = request.data.get('token')
             token = Token.objects.get(key=token_key)
@@ -142,7 +179,6 @@ class Reset_password(generics.UpdateAPIView):
                 "message": "Password reset failed",
                 }, status=status.HTTP_400_BAD_REQUEST)
         except Token.DoesNotExist:
-            # Token not found
             return Response({"code": 401, "message": "Sesion or token disactive"}, status=status.HTTP_401_UNAUTHORIZED)
         except:
             return Response({
@@ -150,12 +186,9 @@ class Reset_password(generics.UpdateAPIView):
                 "message": "Server error"
                 }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+class Reset_password_request(APIView):
 
-class Reset_password_request(generics.UpdateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
-
-    def put(self, request):
+    def patch(self, request):
         try:
             email = request.data.get('email')
             user = CustomUser.objects.get(email=email)
@@ -188,72 +221,15 @@ class Reset_password_request(generics.UpdateAPIView):
                 "message": "Server error"
                 }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-
-class User_list(generics.ListAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserExtendedSerializer
-
-    def get(self, request, format=None):
-        try:
-            user = CustomUser.objects.all()
-            serializer = UserExtendedSerializer(user, many=True)
-            return Response({
-                "code": 200,
-                "user": serializer.data,
-                }, status=status.HTTP_200_OK)
-        except:
-            return Response({
-                "code": 500,
-                "message": "Server error"
-                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-
-class Distance_sum_update(generics.UpdateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserExtendedSerializer
-
-    def get(self, request):
-        try:
-            id = request.data.get('userID')
-            user = CustomUser.objects.get(id=id)
-            serializer = UserExtendedSerializer(user, data=request.data, partial=True)
-            if serializer.is_valid():
-                return Response({
-                        "code": 200,
-                        "distance_sum": serializer.data.get("distance_sum"),
-                        }, status=status.HTTP_200_OK)
-        except:
-            return Response({
-                "code": 500,
-                "message": "Server error"
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def put(self, request):
-        try:
-            id = request.data.get('id')
-            user = CustomUser.objects.get(id=id)
-            serializer = UserExtendedSerializer(user, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(status=status.HTTP_200_OK)
-        except:
-            return Response({
-                "code": 500,
-                "message": "Server error"
-                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-
-class Route_created_count_update(generics.UpdateAPIView):
-    queryset = UserObjectSearch.objects.all()
-    serializer_class = UserObjectSearchExtendedSerializer
+class Route_created_count_update(APIView):
 
     def post(self, request):
         try:
-            object_id = request.data.get("object_id")
-            user = request.data.get("user")
+            user = request.data.get('user')
+            object_id = request.data.get('object_id')
             try:
-                Object.objects.get(id=object_id)
                 CustomUser.objects.get(id=user)
+                Object.objects.get(id=object_id)
             except Object.DoesNotExist:
                 raise serializers.ValidationError("Object does not exist.")
             except CustomUser.DoesNotExist:
@@ -277,6 +253,22 @@ class Route_created_count_update(generics.UpdateAPIView):
             if serializer.is_valid():
                 serializer.save()
                 return Response(status=status.HTTP_200_OK)
+        except:
+            return Response({
+                "code": 500,
+                "message": "Server error"
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class User_list(generics.ListAPIView):
+
+    def get(self, request):
+        try:
+            user = CustomUser.objects.all()
+            serializer = UserExtendedSerializer(user, many=True)
+            return Response({
+                "code": 200,
+                "user": serializer.data,
+                }, status=status.HTTP_200_OK)
         except:
             return Response({
                 "code": 500,
